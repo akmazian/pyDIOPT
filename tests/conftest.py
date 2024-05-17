62d7077fa7a07cd3fde99bdeee5004d1e9d28c30
akmazian <azz@berkeley.edu>
2024-05-16 21:01:04 -0700

    added sample test cases

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.fixture
+def sample_hsapiens_genes():
+    return [
+        "ENSG00000012048",
+        "ENSG00000141510",
+        "ENSG00000146648",
+        "ENSG00000136997",
+        "ENSG00000130203",
+    ]
